b537061170a1d20c02e8244d96a199295c4293e3
Nikhil <nikhilgautam0216@gmail.com>
2024-08-12 22:31:36 +0530

    initalized project

---
+++ client/src/app/services/form.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+ 
+  http = inject(HttpClient);
+
+
+  submitForm(fromdata:any):Observable<any>{
+    return this.http.post<any>(`http://172.16.1.60:8080/save/employee`,fromdata)
+  }
+
+  fetchData():Observable<any>{
+    return this.http.get<any>(`http://172.16.1.60:8080/get/employee`)
+  }
+
+  deleteEmp(Emp_id:any):Observable<any>{
+    return this.http.delete<any>(`http://172.16.1.60:8080/delete/employee/${Emp_id}`)
+  }
+}
